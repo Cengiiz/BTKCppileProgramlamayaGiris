b80b933711a9c35cac8ed1d9b878db9329fafc77
Cengiiz <dadem64@gmail.com>
2023-02-04 16:15:56 +0300

    ders-2.1/2.3

---
+++ Degiskenler/Degiskenler.cpp
@@ -0,0 +1,20 @@
+﻿// Degiskenler.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	/*int sayi1, sayi2, sonuc;
+	sayi1 = 5;
+	sayi2 = 10;
+	sonuc = sayi1 + sayi2;*/
+	
+
+	int Sayi = 5;
+	int sayi = 10;
+
+	std::cout << Sayi;
+	std::cout << sayi;
+}
+
